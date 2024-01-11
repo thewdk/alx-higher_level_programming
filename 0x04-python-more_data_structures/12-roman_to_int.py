63e23f595f0cfe76d5037a907bd2a3e555b21972
Daniel Watchman <dannietortee@gmail.com>
2024-01-11 02:52:32 +0100

    0x04. Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,39 +1,14 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str) or not roman_string:
+    if not roman_string or type(roman_string) != str:
         return 0
-
-    valid_chars = set('IVXLCDM')
-
-    for char in roman_string:
-        if char not in valid_chars:
-            return 0
-
-    total = 0
-
-    num_dict = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000
-    }
-
-    i = 0
-    while i < len(roman_string):
-        a = num_dict[roman_string[i]]
-        if i + 1 < len(roman_string):
-            b = num_dict[roman_string[i + 1]]
-            if a < b:
-                total = total + (a - b)
-                i += 2
-            else:
-                total = total + a
-                i += 1
+    roman_d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman_n = 0
+    for j in range(len(roman_string)):
+        if j > 0 and roman_d[roman_string[j]] > roman_d[roman_string[j - 1]]:
+            roman_n += roman_d[roman_string[j]] - 2 * \
+                    roman_d[roman_string[j - 1]]
         else:
-            total = total + a
-            i += 1
+            roman_n += roman_d[roman_string[j]]
+    return roman_n
 
-    return total
