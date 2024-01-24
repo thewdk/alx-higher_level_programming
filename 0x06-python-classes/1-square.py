8bef586bd1ad193e43b360ab09f282b6b8d8fef1
Daniel Watchman <dannietortee@gmail.com>
2024-01-24 02:13:30 +0100

    Add Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This is a Square class that creates a square object and initializes with size
+"""
+
+
+class Square:
+    """
+    Initialize Square object with private attribute size
+    """
+    def __init__(self, size):
+        self.__size = size
