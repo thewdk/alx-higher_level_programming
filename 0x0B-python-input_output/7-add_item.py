a432a0c42379eb9bdcf6d765768159ecb9472546
Daniel Watchman <dannietortee@gmail.com>
2024-02-07 03:47:56 +0100

    adds task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
-"""initializate"""
-import json
+"""
+
+- Add items and save them in a file
+"""
+import sys
 
 
-def save_to_json_file(my_obj, filename):
-    """writes an Object to a text file"""
-    with open(filename, 'w') as f:
-        file_json = json.dumps(my_obj)
-        f.write(file_json)
-    f.close()
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+if __name__ == '__main__':
+    try:
+        item_list = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        item_list = []
+
+    item_list.extend(sys.argv[1:])
+
+    save_to_json_file(item_list, "add_item.json")
