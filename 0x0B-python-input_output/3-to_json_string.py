fcfe618d9a602f29428b26c8b9041a398df0a4ce
Daniel Watchman <dannietortee@gmail.com>
2024-02-07 03:55:12 +0100

    adds task 5

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """initializate"""
+import json
 
 
-def write_file(filename="", text=""):
-    """function that writes a string to a text file (UTF8)
-     and returns the number of characters written"""
-    with open(filename, "w", encoding="utf-8") as f:
-        return f.write(text)
+def to_json_string(my_obj):
+    """JSON representation of an object (string)"""
+    return json.dumps(my_obj)
