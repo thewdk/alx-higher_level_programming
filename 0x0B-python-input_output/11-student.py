4ee673f3f9b04e4fe8e81e57205f3a90dcf19013
Daniel Watchman <dannietortee@gmail.com>
2024-02-07 03:37:43 +0100

    adds task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+- class: Student
+- public method: def to_json
+- public method: def reload_from_json
+- instantiation: first name, lastname, age
+"""
+
+
+class Student:
+    """Defines a student object"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a new student
+
+        Args:
+            first_name (str): First name of student
+            last_name (str): Last name of the student
+            age (int): Age of the student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Get a dictionary representation of the student if in attrs"""
+        if attrs is not None:
+            return {attr: getattr(self, attr)
+                    for attr in attrs if hasattr(self, attr)}
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replace all attributes of the student instance with json"""
+        for key, value in json.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
