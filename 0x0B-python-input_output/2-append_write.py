ec5515604ddf26804912e65e28dd8b93fd573ee6
Daniel Watchman <dannietortee@gmail.com>
2024-02-07 03:56:42 +0100

    adds task 2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -2,15 +2,10 @@
 """initializate"""
 
 
-def read_lines(filename="", nb_lines=0):
-    """ reads n lines of a text file """
-    i = len(open(filename).readlines())
-    with open(filename, encoding="utf-8") as f:
-        if nb_lines <= 0 or nb_lines >= i:
-            line = f.read()
-            print(line, end="")
-        else:
-            for _ in range(nb_lines):
-                lines = next(f).strip()
-                print(lines)
+def append_write(filename="", text=""):
+    """function that appends a string at the end of a text
+    file (UTF8) and returns the number of characters added:"""
+    with open(filename, 'a', encoding="utf-8") as f:
+        f.write(text)
     f.close()
+    return len(text)
