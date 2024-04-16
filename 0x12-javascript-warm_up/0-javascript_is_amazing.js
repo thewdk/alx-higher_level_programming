2a1606a8c80fa47cdae5fa675a14475025658f3e
Daniel Watchman <dannietortee@gmail.com>
2024-04-16 14:48:30 +0100

    we coding again... JS

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,3 @@
+#!/usr/bin/node
+const myVar = 'JavaScricpt is amazing';
+console.log(myVar);
---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const argCount = process.argv.lenthn - 2;
+if (argCount === 0) {
+  console.log('No argument');
+} else if (argCount === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Argument found');
+}
