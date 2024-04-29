7df21abb9261a4c7f27fbaa9cd27fa8cbf40713d
Daniel Watchman <dannietortee@gmail.com>
2024-04-29 02:30:10 +0100

    task 5 done

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,20 +1,28 @@
 #!/usr/bin/python3
-# Displays all cities of a given state from the
-# states table of the database hbtn_0e_4_usa.
-# Safe from SQL injections.
-# Usage: ./5-filter_cities.py <mysql username> \
-#                             <mysql password> \
-#                             <database name> \
-#                             <state name searched>
-import sys
+"""
+Module to list all states
+"""
 import MySQLdb
+from sys import argv
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `cities` as `c` \
-                INNER JOIN `states` as `s` \
-                   ON `c`.`state_id` = `s`.`id` \
-                ORDER BY `c`.`id`")
-    print(", ".join([ct[2] for ct in c.fetchall() if ct[4] == sys.argv[4]]))
 
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+
+    cursor = db.cursor()
+
+    query = """
+    SELECT cities.name
+    FROM cities
+    JOIN states
+        ON cities.state_id = states.id
+    WHERE states.name = %s
+    ORDER BY cities.id
+    """
+
+    cursor.execute(query, (argv[4],))
+
+    result = cursor.fetchall()
+
+    cities = ', '.join(city[0] for city in result)
+    print(cities)
