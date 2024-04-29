7810a924ae782cc8c617068330ca46e73c652255
Daniel Watchman <dannietortee@gmail.com>
2024-04-29 02:28:29 +0100

    task 4 done

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,18 +1,27 @@
 #!/usr/bin/python3
-# Lists all cities of the database hbtn_0e_4_usa, ordered by city id.
-# Usage: ./4-cities_by_state.py <mysql username> \
-#                               <mysql password> \
-#                               <database name>
-import sys
+"""
+Module to list all states
+"""
 import MySQLdb
+from sys import argv
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT `c`.`id`, `c`.`name`, `s`.`name` \
-                 FROM `cities` as `c` \
-                INNER JOIN `states` as `s` \
-                   ON `c`.`state_id` = `s`.`id` \
-                ORDER BY `c`.`id`")
-    [print(city) for city in c.fetchall()]
 
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+
+    cursor = db.cursor()
+
+    query = """
+    SELECT cities.id, cities.name, states.name
+    FROM cities
+    JOIN states
+        ON cities.state_id = states.id
+    ORDER BY cities.id
+    """
+
+    cursor.execute(query)
+
+    result = cursor.fetchall()
+
+    for state in result:
+        print(state)
