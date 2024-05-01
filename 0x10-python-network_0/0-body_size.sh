2dc6f335a3951da6972da59c41485fb008ffb854
Daniel Watchman <dannietortee@gmail.com>
2024-05-01 03:38:15 +0100

    task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Get the byte size of the HTTP response header for a given URL.
-curl -s "$1" | wc -c
-
+# Displaying the results of GET method
+curl -sI "$1" | grep -i 'Content-Length' | awk '{print $2}'
