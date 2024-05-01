20f90de66ed93f18df2e99dfcda4701e0e863c34
Daniel Watchman <dannietortee@gmail.com>
2024-05-01 03:43:52 +0100

    task 3 modified

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Display all HTTP methods the server of a given URL will accept.
-curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
-
+# DELETE method using curl
+curl -sI "$1" | grep -i "Allow" | cut -d ' ' -f 2-
