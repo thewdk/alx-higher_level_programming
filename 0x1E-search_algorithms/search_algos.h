7e7b6c9a4f45a21684ee1e6f2f631e0de3b1469e
Daniel Watchman <dannietortee@gmail.com>
2024-05-09 01:31:37 +0100

    add task 0

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,13 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+#include <stdio.h>
+
+/* MAIN FUNCTIONS */
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+/* HELPER FUNCTIONS */
+void print_arr(int *array, size_t start, size_t end);
+
+#endif /* SEARCH_ALGOS_H */
